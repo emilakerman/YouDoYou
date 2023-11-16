ad14fc31437d440b39d4673aa309c40799542753
Joel Navarro <joelnavarro@Joels-MBP.lan>
2023-11-16 19:32:23 +0100

    Created HomeWidget and its components

---
+++ lib/home_screen/widgets/home_header.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:youdoyou/constants/app_colors.dart';
+
+class HomeHeader extends StatelessWidget {
+
+  const HomeHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      //color: AppColors.additional,
+      margin: EdgeInsets.only(top: 10),
+      elevation: 10,
+      child: Container(
+        height: 100,
+        width: double.infinity,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+          children: [
+            Container(
+              height: 90,
+              width: 150,
+              margin: EdgeInsets.only(left: 10, right: 10),
+                decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+              child: const Icon(
+                Icons.account_circle_outlined,
+                color: Colors.grey,
+                size: 70,
+                ),
+            ),
+
+
+            Container(
+              height: 90,
+              width: 150,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+              //margin: const EdgeInsets.only(top: 30),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 30,
+                    width: 100,
+                    child: Text('UserName', ),
+                    ),
+                  SizedBox(
+                    height: 30,
+                    width: 100,
+                    child: Text('My ToDos'),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home_screen/widgets/users_todo_list.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:youdoyou/constants/app_colors.dart';
+
+class ToDoList extends StatelessWidget {
+  const ToDoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.complement,
+      child: Column(
+        children: [
+          Container(
+            height: 30,
+            width: double.infinity,
+            child: Text('To Do List:', textAlign: TextAlign.center,),
+            ),
+          Container(
+            height: 270,
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 70,
+                        child: Icon(Icons.add_a_photo_outlined,
+                        size: 50,
+                        color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
