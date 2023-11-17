5126218877f13760eb10697bb2686bb1068bd402
emilakerman <emil.akerman@iths.se>
2023-11-17 11:49:55 +0100

    fixed issue

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 /// The class defines static constant colors for different reusable colors.
 class AppColors {
   static const primary = Color(0xf0BBBDF6);
-  static const seconday = Color(0xf09893DA);
+  static const secondary = Color(0xf09893DA);
   static const complement = Color(0xf0797A9E);
   static const additional = Color(0xf072727E);
   static const extra = Color(0xf0625F63);
