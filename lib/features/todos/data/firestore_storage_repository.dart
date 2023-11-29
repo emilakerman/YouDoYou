a4c24472577443ad622b56e36ac0cb3cacf759b3
emilakerman <emil.akerman@iths.se>
2023-11-29 12:28:52 +0100

    changed name service to repository

--- lib/features/todos/data/firestore_storage_repository.dart
+++ lib/features/todos/data/firestore_storage_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 class FirebaseStorageRepository {
   final FirebaseStorage _storage = FirebaseStorage.instance;
